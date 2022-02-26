25c29f59b3d5e82be351083a44e4d5ee0e2c31ff
unknown <felix.zimmer@mail.de>
2022-02-26 18:41:11 +0100

    a

--- R/functions.R
+++ R/functions.R
@@ -23,7 +23,7 @@ sim_data = function(sample_size) {
 
 train_nn = function(mod,x,y,loss,epochs = 20,learning_rate=.001,batch_size=nrow(x),metrics=NULL) {
   
-  #batch_size = 32 
+  #batch_size = 32 test
   
   
   if(is.data.frame(x)) x = as.matrix(x)
